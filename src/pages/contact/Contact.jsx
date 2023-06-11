ebddc67f76bf6e9d411821f90923447c90b16a90
Thomas Githuku <thomasngangagithuku@gmail.com>
2023-06-11 16:23:14 +0300

    my final portfolio

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+
+import {
+  FaEnvelopeOpen,
+  FaPhoneSquareAlt,
+  FaFacebookF,
+  FaTwitter,
+  FaYoutube,
+  FaDribbble,
+} from "react-icons/fa";
+
+import { FiSend } from "react-icons/fi";
+import "./contact.css";
+
+const contact = () => {
+  return (
+    <section className="contact section">
+      <h2 className="section__title">
+        Get In <span>Touch</span>
+      </h2>
+
+      <div className="contact__container container grid">
+        <div className="contact__data">
+          <h3 className="contact__title">Don't be shy !</h3>
+
+          <p className="contact__description">
+            Feel free to get in touch with me. I am always open to discussing
+            new projects, creative ideas or opportunities to be part of your
+            visions.
+          </p>
+
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className="info_icon" />
+
+              <div>
+                <span className="info__title">Mail me</span>
+                <h4 className="info__desc">thomasngangagithuku@gmail.com</h4>
+              </div>
+            </div>
+
+            <div className="info__item">
+              <FaPhoneSquareAlt className="info_icon" />
+
+              <div>
+                <span className="info__title">call me</span>
+                <h4 className="info__desc">+254 708 366 961</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact__socials">
+            <a href="http://facebook.com" className="contact__social-link">
+              <FaFacebookF />
+            </a>
+
+            <a href="http://twitter.com" className="contact__social-link">
+              <FaTwitter />
+            </a>
+
+            <a href="http://youtube.com" className="contact__social-link">
+              <FaYoutube />
+            </a>
+
+            <a href="http://dribbbler.com" className="contact__social-link">
+              <FaDribbble />
+            </a>
+          </div>
+        </div>
+
+        <form className="contact__form">
+          <div className="form__input-group">
+            <div className="form__input-div">
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="form__control
+              "
+              />
+            </div>
+
+            <div className="form__input-div">
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="form__control
+              "
+              />
+            </div>
+
+            <div className="form__input-div">
+              <input
+                type="text"
+                placeholder="Your Subject"
+                className="form__control
+              "
+              />
+            </div>
+          </div>
+
+          <div className="form__input-div">
+            <textarea className="form__control textarea"></textarea>
+          </div>
+
+          <button className="button">
+            Send Message
+            <span className=" button__icon contact__button-icon">
+              <FiSend />
+            </span>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default contact;
